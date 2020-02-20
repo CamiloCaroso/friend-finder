9a8272a51830c89baf2bf9f7d25f6eb0c38138fd
Camilo Caroso <camilitosamuel@hotmail.com>
2020-02-20 18:16:29 -0500

    hw

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+const friendsData = [
+    {
+        name: "Peter",
+        score: 13
+    },
+    {
+        name: "Tommy",
+        score: 15
+    },
+    {
+        name: "Antony",
+        score: 11
+    },
+    {
+        name: "Martin",
+        score: 20
+    }
+];
+
+module.exports = friendsData;
